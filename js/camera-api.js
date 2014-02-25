e324bfc0c613bfd390adfab3f3db4cb6fc9c1681
Joacim Lowgren <joacim.lowgren@gmail.com>
2014-02-25 19:57:54 +0100

    Basic image capturing

--- js/camera-api.js
+++ js/camera-api.js
@@ -1,35 +1,47 @@
-function init() {
-  navigator.getUserMedia = (navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia ||
-                            navigator.msGetUserMedia);
+(function () {
 
-  if (navigator.getUserMedia) {
-    var opts = {
-      video: true
-    };
-    navigator.getUserMedia(opts, function (stream) {
-      var video = document.querySelector('video');
-      video.src = window.URL.createObjectURL(stream);
-    }, function (err) {
-      console.error(err);
-    });
+  var main = document.querySelector('.main');
+  var snapshots = document.querySelector('.snapshots');
+  var canvas = document.querySelector('canvas');
+  var context = canvas.getContext('2d');
+  var localMediaStream = null;
 
-    var captureButton = document.querySelector('button');
-    captureButton.addEventListener('click', function (event) {
-      event.preventDefault();
-      captureImage();
-    });
+  function init() {
+    navigator.getUserMedia = (navigator.getUserMedia ||
+                              navigator.webkitGetUserMedia ||
+                              navigator.mozGetUserMedia ||
+                              navigator.msGetUserMedia);
+
+    if (navigator.getUserMedia) {
+      var opts = {
+        video: true
+      };
+      navigator.getUserMedia(opts, function (stream) {
+        localMediaStream = stream;
+        var video = document.querySelector('video');
+        video.src = window.URL.createObjectURL(stream);
+      }, function (err) {
+        console.error(err);
+      });
+
+      var captureButton = document.querySelector('button');
+      captureButton.addEventListener('click', captureImage, false);
+    } else {
+      alert('Your browser is old. :(');
+    }
   }
-  else {
-    alert('Your browser is old. :(');
+
+  function captureImage() {
+    var listItem = document.createElement('li');
+    var image = document.createElement('img');
+
+    if (localMediaStream) {
+      image.src = canvas.toDataURL();
+      listItem.appendChild(image);
+      snapshots.appendChild(listItem);
+    }
   }
-}
 
-function captureImage() {
-  console.log('Capturing image...');
-}
-
-window.onload = function () {
   init();
-};
+
+})();
