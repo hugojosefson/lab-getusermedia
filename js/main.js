77e5bc30beca85fc852beb0b97934f9293338936
Joacim Löwgren <joacim.lowgren@jayway.com>
2014-05-04 17:07:43 +0200

    Add fullscreen button

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function () {
 
   var $video = $('.camera video');
   var $capture = $('.controls .capture-image');
+  var $fullscreen = $('.controls .go-fullscreen');
   var $filters = $('.controls .filter');
   var camera = new Camera($video[0], {
     video: true,
@@ -20,6 +21,17 @@ $(function () {
             .append(image)
         )
     );
+  });
+
+  /* Set fullscreen */
+  $fullscreen.on('click', function () {
+    var video = $video[0];
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    }
+    else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen();
+    }
   });
 
   /* Set filter */
